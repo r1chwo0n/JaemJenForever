ccae42142d0266922eefbb11c7032133c22884ff
r1chwo0n <woonzy09@gmail.com>
2023-01-10 15:53:48 +0700

    lab13_3.cpp completed

--- lab13_3.cpp
+++ lab13_3.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-
 using namespace std;
 
 void mySwap(int &,int &); 
@@ -14,4 +13,11 @@ int main(){
 	cout << "x = " << x << ", y = " << y << "\n";
 	
 	return 0;
+}
+
+void mySwap(int &x, int &y){
+  const int a = x;
+  const int b = y;
+  x = b;
+  y = a;
 }
